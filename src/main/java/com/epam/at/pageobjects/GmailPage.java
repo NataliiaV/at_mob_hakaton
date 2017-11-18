d958113609a0f8444b87c1414e82608bdc71f63a
Natalia <Mykola_Zomchak@epam.com>
2017-11-18 15:53:33 +0200

    Add Emails support

--- src/main/java/com/epam/at/pageobjects/GmailPage.java
+++ src/main/java/com/epam/at/pageobjects/GmailPage.java
@@ -1,6 +1,15 @@
 package com.epam.at.pageobjects;
 
+import elements.Email;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class GmailPage {
 
+    @FindBy(xpath = "contains(@contentDescription, 'Take care of this number:')")
+    WebElement emailWithCode;
 
+    public Email getEmailWithCode(){
+        return new Email(emailWithCode);
+    }
 }

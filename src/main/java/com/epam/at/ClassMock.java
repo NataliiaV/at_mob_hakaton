20bce56d23c43e236016caa9bb4f46dd307f8175
MonchynR <monchynroman@gmail.com>
2017-11-18 12:40:52 +0200

    Add desired capabilities

--- src/main/java/com/epam/at/ClassMock.java
+++ src/main/java/com/epam/at/ClassMock.java
@@ -1,4 +1,44 @@
 package com.epam.at;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ClassMock {
+    private String reportDirectory = "reports";
+    private String reportFormat = "xml";
+    private String testName = "Untitled";
+    protected AndroidDriver<AndroidElement> driver = null;
+
+    DesiredCapabilities dc = new DesiredCapabilities();
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        dc.setCapability("reportDirectory", reportDirectory);
+        dc.setCapability("reportFormat", reportFormat);
+        dc.setCapability("testName", testName);
+        dc.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+        dc.setCapability(MobileCapabilityType.APP, new File("src\\main\\resources\\app-release.apk").getAbsolutePath());
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.hackaton.epam.tahackaton");
+        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
+        driver = new AndroidDriver<AndroidElement>(new URL("http://localhost:4723/wd/hub"), dc);
+    }
+
+    @Test
+    public void testUntitled() {
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
